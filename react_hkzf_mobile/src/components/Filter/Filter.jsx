d8b16f7d827beba668d40b835e24c0084a44766f
L-cuitain <948527099@qq.com>
2021-07-06 17:18:35 +0800

    update: FindHouse页面与组件更新

---
+++ react_hkzf_mobile/src/components/Filter/Filter.jsx
@@ -0,0 +1,25 @@
+//引入React
+import React , { useState } from 'react';
+
+//引入FilterTitle
+import FilterTitle from '../FilterTitle/FilterTitle';
+
+//引入样式
+import Style from './Filter.module.css';
+
+function Filter(){
+    return (
+        <div className={Style.root}>
+            {/* 前三个菜单的遮罩层 */}
+            <div className={Style.content}>
+                {/* 标题栏 */}
+                <FilterTitle />
+                {/* 前三个菜单对应内容 */}
+                {/* 最后一个菜单对应内容 */}
+            </div>
+            
+        </div>
+    );
+}
+
+export default Filter;
---
+++ react_hkzf_mobile/src/components/FilterTitle/FilterTitle.jsx
@@ -0,0 +1,36 @@
+//引入React
+import React , { useState } from 'react';
+
+//引入Flex
+import { Flex } from 'antd-mobile';
+
+//引入样式
+import Style from './FilterTitle.module.css';
+
+//条件筛选栏标题数组
+const titleList = [
+    {title: '区域' , type: 'area'},
+    {title: '方式' , type: 'mode'},
+    {title: '租金' , type: 'price'},
+    {title: '筛选' , type: 'more'}
+]
+
+function FilterTitle(){
+    return (
+        <Flex align="center" className={Style.root}>
+            {/* 选中类名: selected */}
+            {
+                titleList.map(item => (
+                    <Flex.Item key={item.type}>
+                        <span className={[Style.dropdown].join(' ')}>
+                            <span>{item.title}</span>
+                            <i className="iconfont icon-arrow"></i>
+                        </span>
+                    </Flex.Item>
+                ))
+            }
+        </Flex>
+    );
+}
+
+export default FilterTitle;
